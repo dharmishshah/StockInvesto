519fe7f73091f30bf0bc05ec7cc8a6fd823c6105
Animesh Mane <mane.an@husky.neu.edu>
2018-12-06 17:52:37 -0500

    New Commit

--- src/main/java/stocks/view/ApplicationMain.java
+++ src/main/java/stocks/view/ApplicationMain.java
@@ -6,13 +6,15 @@ import stocks.view.portfolio.PortfolioMain;
 import stocks.view.stock.StockMain;
 
 /**
- * The following class represents the Main page of the GUI Application for the system.
- * The main frame consists of the profile, stock and exit button.
+ * The following class represents the Main page of the GUI Application for the system. The main
+ * frame consists of the profile, stock and exit button.
  */
 
 public class ApplicationMain extends javax.swing.JFrame {
 
   GUIController controller;
+  //The following variable represents the panel.
+  private javax.swing.JSplitPane jSplitPane1;
 
   /**
    * The following constructor creates the new form ApplicationMain.
@@ -29,6 +31,19 @@ public class ApplicationMain extends javax.swing.JFrame {
   @SuppressWarnings("unchecked")
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
+
+    //The following variable represents the exit button.
+    javax.swing.JButton exitBtn;
+    //The following variable represents the investment button.
+    javax.swing.JButton investmentBtn;
+    //The following variable represents the panel.
+    javax.swing.JPanel jPanel1;
+    //The following variable represents the panel.
+    javax.swing.JPanel jPanel2;
+    //The following variable represents the profile button.
+    javax.swing.JButton portfolioBtn;
+    //The following variable represents the stock button.
+    javax.swing.JButton stockBtn;
 
     jSplitPane1 = new javax.swing.JSplitPane();
     jPanel1 = new javax.swing.JPanel();
@@ -74,8 +89,8 @@ public class ApplicationMain extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                             .addContainerGap()
-                            .addGroup(jPanel1Layout.createParallelGroup
-                                    (javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(jPanel1Layout.createParallelGroup(
+                                    javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(investmentBtn,
                                             javax.swing.GroupLayout.DEFAULT_SIZE,
                                             javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -135,10 +150,11 @@ public class ApplicationMain extends javax.swing.JFrame {
     );
 
     pack();
-  }// </editor-fold>//GEN-END:initComponents
+  }
 
   /**
    * The following method represents the action performed for the portfolio button.
+   *
    * @param evt the event instance performed on click of the button
    */
   private void portfolioBtnActionPerformed(java.awt.event.ActionEvent evt) {
@@ -148,6 +164,7 @@ public class ApplicationMain extends javax.swing.JFrame {
 
   /**
    * The following method represents the action performed when the stock button is clicked.
+   *
    * @param evt the event instance of the stock button.
    */
   private void stockBtnActionPerformed(java.awt.event.ActionEvent evt) {
@@ -157,6 +174,7 @@ public class ApplicationMain extends javax.swing.JFrame {
 
   /**
    * The following method represents the action performed when the investment button is clicked.
+   *
    * @param evt the event instance of the stock button.
    */
   private void investmentBtnActionPerformed(java.awt.event.ActionEvent evt) {
@@ -166,6 +184,7 @@ public class ApplicationMain extends javax.swing.JFrame {
 
   /**
    * The following method represents the action performed when the exit button is clicked.
+   *
    * @param evt the event instance of the stock button.
    */
   private void exitBtnActionPerformed(java.awt.event.ActionEvent evt) {
@@ -174,9 +193,10 @@ public class ApplicationMain extends javax.swing.JFrame {
 
   /**
    * The following method represents the main method of the GUI application.
+   *
    * @param args the command line arguments
    */
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     try {
       for (javax.swing.UIManager.LookAndFeelInfo info
               : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -206,18 +226,4 @@ public class ApplicationMain extends javax.swing.JFrame {
       }
     });
   }
-  //The following variable represents the exit button.
-  private javax.swing.JButton exitBtn;
-  //The following variable represents the investment button.
-  private javax.swing.JButton investmentBtn;
-  //The following variable represents the panel.
-  private javax.swing.JPanel jPanel1;
-  //The following variable represents the panel.
-  private javax.swing.JPanel jPanel2;
-  //The following variable represents the panel.
-  private javax.swing.JSplitPane jSplitPane1;
-  //The following variable represents the profile button.
-  private javax.swing.JButton portfolioBtn;
-  //The following variable represents the stock button.
-  private javax.swing.JButton stockBtn;
 }
